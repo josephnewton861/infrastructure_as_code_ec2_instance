01439163a5d464dd405d46ab369132302595f1b8
AzureAD\JosephNewton <joe@theinsightsfamily.com>
2022-07-11 10:17:25 +0100

    created an internet gateway with security group

--- main.tf
+++ main.tf
@@ -32,3 +32,76 @@ resource "aws_subnet" "private_subnet" {
       "Name" = "private_subnet-${count.index}"
     }
 }
+
+# Create an internet gateway
+resource "aws_internet_gateway" "my_internet_gateway" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = "my_internet_gateway"
+  }
+}
+
+# Create a route table public and private
+resource "aws_route_table" "public_route_table_test" {
+    vpc_id = aws_vpc.test_vpc.id
+
+    route {
+        gateway_id = aws_internet_gateway.my_internet_gateway.id
+        cidr_block = "0.0.0.0/0"
+    }
+    
+    route {
+        #ipv6_cidr_blocks
+        ipv6_cidr_block        = "::/0"
+        gateway_id = aws_internet_gateway.my_internet_gateway.id
+    }
+    tags = {
+        Name = "public_route_table_test"
+    }
+}
+
+# Create a security group
+resource "aws_security_group" "allow_web" {
+    name        = "allow_web"
+    description = "Allow web inbound traffic"
+    vpc_id      = aws_vpc.test_vpc.id
+
+    ingress {
+        description      = "HTTPS"
+        from_port        = 443
+        to_port          = 443
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        description      = "HTTP"
+        from_port        = 80
+        to_port          = 80
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description      = "SSH"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "allow_web"
+    }
+}
+
+
